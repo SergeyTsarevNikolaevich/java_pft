3bdaf83a4543415084a45046a8e28c5a087d2583
akademik2011 <akademik2011@gmail.com>
2019-04-14 20:51:40 +0300

    d

--- addressbooks-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest2.java
+++ addressbooks-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest2.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
